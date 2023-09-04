de796dd8268d5c5e40eb796b86b7e0c593ac0265
@farihaaktar1908 <farihaaktar1908@gmail.com>
2023-09-04 13:11:23 +0600

    changes settings folder

--- fariha_portfolio/settings.py
+++ fariha_portfolio/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'fariha_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,9 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 STATICFILES_DIRS = [
     BASE_DIR/'static',
